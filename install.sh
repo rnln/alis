22fc603f34b9621eccda5c53c922d00f6424aeef
Roman Ilin <romanilin@pm.me>
2021-01-13 16:29:51 +0300

    remove some functionality for tests

--- install.sh
+++ install.sh
@@ -283,18 +283,18 @@ install_post () {
     log 'mirrors configuring'
     sudo pacman -Syyu --noconfirm --needed
     install_packages reflector
-    sudo sh -c 'reflector --latest 20 --sort rate -c Austria,Belarus,Czechia,Denmark,Finland,Germany,Hungary,Latvia,Lithuania,Moldova,Norway,Poland,Romania,Russia,Slovakia,Sweden,Ukraine --protocol https > /etc/pacman.d/mirrorlist'
+    # sudo sh -c 'reflector --latest 20 --sort rate -c Austria,Belarus,Czechia,Denmark,Finland,Germany,Hungary,Latvia,Lithuania,Moldova,Norway,Poland,Romania,Russia,Slovakia,Sweden,Ukraine --protocol https > /etc/pacman.d/mirrorlist'
     log -f 'mirrors configuring'
 
-    log 'yay installation'
-    install_packages base-devel git
-    tempdir="$(mktemp -d)"
-    git clone https://aur.archlinux.org/yay.git "$tempdir"
-    sh -c "cd '$tempdir' && makepkg -si --noconfirm --needed"
-    rm -rf "$tempdir"
-    mkdir "$HOME/.gnupg"
-    echo 'keyserver hkps://keyserver.ubuntu.com' > "$HOME/.gnupg/gpg.conf"
-    log -f 'yay installation'
+    # log 'yay installation'
+    # install_packages base-devel git
+    # tempdir="$(mktemp -d)"
+    # git clone https://aur.archlinux.org/yay.git "$tempdir"
+    # sh -c "cd '$tempdir' && makepkg -si --noconfirm --needed"
+    # rm -rf "$tempdir"
+    # mkdir "$HOME/.gnupg"
+    # echo 'keyserver hkps://keyserver.ubuntu.com' > "$HOME/.gnupg/gpg.conf"
+    # log -f 'yay installation'
 
     log 'fonts installation'
     install_packages noto-fonts noto-fonts-emoji noto-fonts-cjk
