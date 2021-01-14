360f11bde678cae3386a8bb9b2d6b61ef7e13541
Roman Ilin <romanilin@pm.me>
2021-01-14 16:54:20 +0300

    replace Oh My Zsh with starship

--- install.sh
+++ install.sh
@@ -1,18 +1,39 @@
 #!/bin/sh
-
+#
+# Run script via curl:
+#   sh -c "$(curl -fsSL https://gitlab.com/romanilin/alis/-/raw/main/install.sh)"
+# Arguments:
+#   -p, --post  Start post-install (grub, swapiness, pacman configuring, GNOME installation, etc.)
+#               By deafult script starts base Arch Linux installation with NetworkManager
+#   --no-lts    Install linux package instead of linux-lts
+# Example:
+#   sh -c "$(curl -fsSL https://gitlab.com/romanilin/alis/-/raw/main/install.sh)" '' --unattended
+#
 set -e
+
+for option in "$@"; do
+    shift
+    case "$option" in
+        '--no-lts')
+            set -- "$@" '-l' ;;
+        *)
+            set -- "$@" "$option"
+    esac
+done
+
+mode=base
+keep_color_scheme=false
+lts=true
 
 OPTIND=1
 
 while getopts 'p' option; do
     case "$option" in
-        p) mode='post';;
-        c) keep_color_scheme=true;;
+        p) mode='post' ;;
+        c) keep_color_scheme=true
+        l) lts=false
     esac
 done
-
-mode=${mode:-base}
-keep_color_scheme=${keep_color_scheme:-false}
 
 shift $((OPTIND-1))
 [ "${1:-}" = '--' ] && shift
@@ -29,10 +50,8 @@ log () {
 
     while getopts 'fd:' option; do
         case "$option" in
-            f)  status='Finished'
-                ;;
-            d)  depth=$OPTARG
-                ;;
+            f) status='Finished' ;;
+            d) depth=$OPTARG
         esac
     done
 
@@ -68,43 +87,42 @@ setup_terminal_colors () {
 
 
 setup_color_scheme () {
-    if [ "$keep_color_scheme" = true ]; then
-        BLACK='#000000'
-        RED='#aa0000'
-        GREEN='#00aa00'
-        YELLOW='#aa5500'
-        BLUE='#0000aa'
-        MAGENTA='#aa00aa'
-        CYAN='#00aaaa'
-        WHITE='#aaaaaa'
-        BLACK_BRIGHT='#555555'
-        RED_BRIGHT='#ff5555'
-        GREEN_BRIGHT='#55ff55'
-        YELLOW_BRIGHT='#ffff55'
-        BLUE_BRIGHT='#5555ff'
-        MAGENTA_BRIGHT='#ff55ff'
-        CYAN_BRIGHT='#55ffff'
-        BACKGROUND_HIGHLIGHT='#000055'
-    else
-        BLACK='#121212'
-        RED='#ff746c'
-        GREEN='#00bb23'
-        YELLOW='#a4a600'
-        BLUE='#a594ff'
-        MAGENTA='#ff5aff'
-        CYAN='#00b4b5'
-        WHITE='#ffffff'
-        BLACK_BRIGHT='#555555'
-        RED_BRIGHT=$RED
-        GREEN_BRIGHT=$GREEN
-        YELLOW_BRIGHT=$YELLOW
-        BLUE_BRIGHT=$BLUE
-        MAGENTA_BRIGHT=$MAGENTA
-        CYAN_BRIGHT=$CYAN
-        BACKGROUND_HIGHLIGHT='#264f78'
-    fi
-
+    BLACK='#000000'
+    RED='#aa0000'
+    GREEN='#00aa00'
+    YELLOW='#aa5500'
+    BLUE='#0000aa'
+    MAGENTA='#aa00aa'
+    CYAN='#00aaaa'
+    WHITE='#aaaaaa'
+    BLACK_BRIGHT='#555555'
+    RED_BRIGHT='#ff5555'
+    GREEN_BRIGHT='#55ff55'
+    YELLOW_BRIGHT='#ffff55'
+    BLUE_BRIGHT='#5555ff'
+    MAGENTA_BRIGHT='#ff55ff'
+    CYAN_BRIGHT='#55ffff'
     WHITE_BRIGHT='#ffffff'
+    BACKGROUND_HIGHLIGHT='#000055'
+
+    # BLACK='#121212'
+    # RED='#ff746c'
+    # GREEN='#00bb23'
+    # YELLOW='#a4a600'
+    # BLUE='#a594ff'
+    # MAGENTA='#ff5aff'
+    # CYAN='#00b4b5'
+    # WHITE='#ffffff'
+    # BLACK_BRIGHT='#555555'
+    # RED_BRIGHT=$RED
+    # GREEN_BRIGHT=$GREEN
+    # YELLOW_BRIGHT=$YELLOW
+    # BLUE_BRIGHT=$BLUE
+    # MAGENTA_BRIGHT=$MAGENTA
+    # CYAN_BRIGHT=$CYAN
+    # WHITE_BRIGHT='#ffffff'
+    # BACKGROUND_HIGHLIGHT='#264f78'
+
     BACKGROUND=$BLACK
     FOREGROUND=$WHITE
 
@@ -116,7 +134,7 @@ install_packages () {
     case $1 in
         -a) shift
             yay -S --noconfirm --needed "$@" ;;
-        *) sudo pacman -S --noconfirm --needed "$@" ;;
+        *) sudo pacman -S --noconfirm --needed "$@"
     esac
 }
 
@@ -125,15 +143,15 @@ system_errors () {
     local sudo=''
     while getopts 's' option; do
         case "$option" in
-            s) sudo='sudo ' ;;
+            s) sudo='sudo '
         esac
     done
 
     echo -e "${ES_BOLD}System errors information${ES_RESET}."
     echo -e "${ES_BOLD}${ES_CYAN}${sudo}systemctl --failed${ES_RESET}:"
-    $sudo PAGER= systemctl --failed
+    PAGER= $sudo systemctl --failed
     echo -e "${ES_BOLD}${ES_CYAN}${sudo}journalctl -p 3 -xb${ES_RESET}:"
-    $sudo PAGER= journalctl -p 3 -xb
+    PAGER= $sudo journalctl -p 3 -xb
 
     while true; do
         read -e -p "Clear these logs? [Y/n] " yn
@@ -142,7 +160,6 @@ system_errors () {
             *)  $sudo systemctl reset-failed
                 $sudo journalctl --vacuum-time=1s
                 break
-                ;;
         esac
     done
 }
@@ -216,7 +233,7 @@ install_base () {
     pacstrap /mnt base linux-lts linux-firmware # linux-lts-headers
     log -f 'essential packages installation'
 
-    log 'system configuration'
+    log 'system configuring'
     genfstab -U /mnt >> /mnt/etc/fstab
     # arch-chroot /mnt ln --force --symbolic /usr/share/zoneinfo/Europe/Moscow /etc/localtime
     ln --force --symbolic /mnt/usr/share/zoneinfo/Europe/Moscow /mnt/etc/localtime
@@ -224,16 +241,16 @@ install_base () {
     sed -i 's/^#\(\(en_US\|ru_RU\)\.UTF-8 UTF-8\)/\1/' /mnt/etc/locale.gen
     arch-chroot /mnt locale-gen
     echo LANG=en_US.UTF-8 > /mnt/etc/locale.conf
-    log -f 'system configuration'
+    log -f 'system configuring'
 
-    log 'network configuration'
+    log 'network configuring'
     echo "$hostname" > /mnt/etc/hostname
     echo '127.0.0.1 localhost' > /mnt/etc/hosts
     echo '::1 localhost' >> /mnt/etc/hosts
     echo "127.0.1.1 $hostname.localdomain $hostname" >> /mnt/etc/hosts
     arch-chroot /mnt pacman -S --noconfirm --needed networkmanager
     arch-chroot /mnt systemctl enable NetworkManager
-    log -f 'network configuration'
+    log -f 'network configuring'
 
     log 'users configuring'
     arch-chroot /mnt sh -c "(echo '$root_password'; echo '$root_password') | passwd >/dev/null"
@@ -241,7 +258,7 @@ install_base () {
     arch-chroot /mnt sh -c "(echo '$user_password'; echo '$user_password') | passwd >/dev/null $user_username"
     arch-chroot /mnt pacman -S --noconfirm --needed sudo
     sed -i 's/^# \(%wheel ALL=(ALL) ALL\)/\1/' /mnt/etc/sudoers
-    log -f 'users configuration'
+    log -f 'users configuring'
 
     log 'boot loader installation and configuring'
     arch-chroot /mnt pacman -S --noconfirm --needed grub efibootmgr
@@ -307,9 +324,9 @@ install_post () {
 
     log 'zsh installation'
     install_packages zsh
-    [ -d "$HOME/.oh-my-zsh" ] || sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" '' --unattended
+    # [ -d "$HOME/.oh-my-zsh" ] || sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" '' --unattended
+    curl -fsSL https://starship.rs/install.sh | bash -s -- -f
     sudo chsh -s "$(which zsh)" "$(whoami)"
-    history -c
     rm "$HOME/.bash"*
     log -f 'zsh installation'
 
@@ -333,6 +350,7 @@ install_post () {
     install_packages \
         man \
         vim \
+        tilix \
         # wget \
         # nmap \
         # imagemagick \
@@ -403,8 +421,8 @@ install_post () {
     dconf write /org/gnome/terminal/legacy/profiles:/:$terminal_profile/cell-height-scale 1.15
     dconf write /org/gnome/terminal/legacy/profiles:/:$terminal_profile/scrollback-lines 1000000
 
-    # sudo sed -i 's/^Exec=tilix$/& --maximize/' /usr/share/applications/com.gexperts.Tilix.desktop
-    # sudo sed -i '/DBusActivatable/d' /usr/share/applications/com.gexperts.Tilix.desktop
+    sudo sed -i 's/^Exec=tilix$/& --maximize/' /usr/share/applications/com.gexperts.Tilix.desktop
+    sudo sed -i '/DBusActivatable/d' /usr/share/applications/com.gexperts.Tilix.desktop
     dconf write /com/gexperts/Tilix/app-title "'\${appName}: \${title}'"
     dconf write /com/gexperts/Tilix/unsafe-paste-alert false
     dconf write /com/gexperts/Tilix/window-save-state true
