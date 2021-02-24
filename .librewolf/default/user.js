8c8c59a912188e5f9ba33861d9a5aadb6b80b0dd
Roman Ilin <romanilin@pm.me>
2021-02-24 16:00:03 +0000

    Minor changes

--- .librewolf/default/user.js
+++ .librewolf/default/user.js
@@ -8,6 +8,7 @@ user_pref("browser.region.network.url", "");
 user_pref("browser.region.update.enabled", false);
 user_pref("browser.startup.homepage", "about:blank");
 user_pref("browser.startup.page", 3);
+user_pref("browser.tabs.animate", false);
 user_pref("browser.urlbar.decodeURLsOnCopy", true);
 user_pref("browser.urlbar.dnsResolveSingleWordsAfterSearch", 0);
 user_pref("browser.zoom.siteSpecific", false);
@@ -30,27 +31,27 @@ user_pref("permissions.default.camera", 2);
 user_pref("permissions.default.desktop-notification", 2);
 user_pref("permissions.default.microphone", 2);
 user_pref("privacy.firstparty.isolate", true);
+user_pref("privacy.resistFingerprinting", false);
 user_pref("security.ask_for_password", 1);
 user_pref("signon.generation.enabled", false);
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("ui.prefersReducedMotion", 1);
-user_pref("browser.tabs.animate", false);
 user_pref("ui.systemUsesDarkTheme", 1);
 
+user_pref("browser.bookmarks.addedImportButton", true);
 user_pref("browser.ctrlTab.recentlyUsedOrder", false);
 user_pref("browser.pageActions.persistedActions", "{\"version\":1,\"ids\":[\"bookmark\",\"pinTab\",\"bookmarkSeparator\",\"copyURL\",\"emailLink\",\"addSearchEngine\",\"screenshots_mozilla_org\"],\"idsInUrlbar\":[]}");
 user_pref("browser.policies.runOncePerModification.extensionsInstall", "[$xpi_list]");
+user_pref("browser.toolbars.bookmarks.visibility", "always");
 user_pref("browser.uiCustomization.state", "{\"placements\":{\"widget-overflow-fixed-list\":[],\"nav-bar\":[\"back-button\",\"forward-button\",\"stop-reload-button\",\"urlbar-container\",\"fxa-toolbar-menu-button\",\"downloads-button\",\"ublock0_raymondhill_net-browser-action\",\"add-ons-button\",\"panic-button\",\"preferences-button\"],\"toolbar-menubar\":[\"menubar-items\"],\"TabsToolbar\":[\"tabbrowser-tabs\",\"new-tab-button\",\"alltabs-button\"],\"PersonalToolbar\":[\"managed-bookmarks\",\"personal-bookmarks\"]},\"seen\":[],\"dirtyAreaCache\":[],\"currentVersion\":16}");
 user_pref("browser.uidensity", 1);
+user_pref("devtools.theme", "dark");
+// user_pref("devtools.toolbox.host", "right");+
+// user_pref("devtools.toolbox.sidebar.width", 960);+
 user_pref("extensions.activeThemeID", "firefox-compact-dark@mozilla.org");
+user_pref("findbar.highlightAll", true);
 user_pref("font.name-list.monospace.x-western", "JetBrains Mono, Noto Sans Mono, monospace, Noto Color Emoji");
 user_pref("font.size.monospace.x-western", 12);
-user_pref("devtools.theme", "dark");
-// user_pref("devtools.toolbox.host", "right");
-// user_pref("devtools.toolbox.sidebar.width", 960);
-// user_pref("layout.css.backdrop-filter.enabled", true);
-// user_pref("gfx.webrender.all", true);
+// user_pref("gfx.webrender.all", true);+
 user_pref("identity.fxaccounts.account.device.name", "$USER@$HOST");
-user_pref("findbar.highlightAll", true);
-user_pref("browser.toolbars.bookmarks.visibility", "always");
-user_pref("browser.bookmarks.addedImportButton", true);
+// user_pref("layout.css.backdrop-filter.enabled", true);+
