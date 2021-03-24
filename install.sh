712b12b10c4526b41fcf2e34633a4529f54886a8
Roman Ilin <romanilin@pm.me>
2021-03-25 02:43:48 +0300

    Minor changes

--- install.sh
+++ install.sh
@@ -500,7 +500,7 @@ function update_configuration () {
 	# done
 	# log -f -i 1 'Firefox add-ons installation'
 
-	if [ ! -f "$XDG_DATA_HOME"/keepassxc/database.kdbx ] || ask 'Update KeePassXC database'; then
+	if [ ! -f "$XDG_DATA_HOME"/keepassxc/database.kdbx ] || ask -n 'Update KeePassXC database'; then
 		rm -f "$XDG_DATA_HOME"/keepassxc/database.kdbx
 		mkdir -p "$XDG_DATA_HOME"/keepassxc
 		cp "$tempdir"/.local/share/keepassxc/database.kdbx "$XDG_DATA_HOME"/keepassxc/database.kdbx
